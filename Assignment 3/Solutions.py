a32df54ec61ade7c9a627a9b04ace0445da0bc7b
Max9924 <mopoku075@gmail.com>
2025-04-16 18:40:15 +0000

    Update Solutions.py

--- Assignment 3/Solutions.py
+++ Assignment 3/Solutions.py
@@ -3,30 +3,54 @@ Solutions to assignment 3
 """
 
 """
-1.Write a Python program to reverse the string "Programming". Print the reversed string.
+1.Write a Python progra+23353930251m to reverse the string "Programming". Print the reversed string.
 Hint: Use string slicing or a loop.
 """
+original_string = "Programming"
 
+reversed_string = original_string[::-1]
+
+print("Reversed string:", reversed_string)
 
 
 """
 2.Create a Python program that takes a user’s full name as input and prints the initials in uppercase.
 Example: Input: "john doe", Output: "J.D."
 """
+full_name = input("Enter your full name: ")
 
+name_parts = full_name.strip().split()
+
+initials = ".".join([part[0].upper() for part in name_parts]) + "."
+
+print("Initials:", initials)
 
 
 """
 3.Write a Python program to check if a given string is a palindrome. A palindrome reads the same forwards
 and backward (e.g., "radar", "level"). Hint: Compare the string with its reverse.
 """
+text = input("Enter a string: ")
 
+clean_text = text.replace(" ", "").lower()
+
+if clean_text == clean_text[::-1]:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
 
 
 """
 4.Create a Python program that asks the user to enter a sentence and counts the number of words in the sentence.
 Hint: Use the split() method to break the string into words.
 """
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+word_count = len(words)
+
+print("Number of words in the sentence:", word_count)
 
 
 
@@ -34,3 +58,8 @@ Hint: Use the split() method to break the string into words.
 5.Write a Python program to replace all occurrences of "is" with "was" in the string "This is a string and it
 is an example." Print the modified string.
 """
+text = "This is a string and it is an example."
+
+modified_text = text.replace("is", "was")
+
+print("Modified string:", modified_text)
